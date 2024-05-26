4776e4d5994847c4a8dbae919c1607993043e51a
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-05-26 14:25:36 -0300

    atualizações dos exercícios

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// movendo pelo DOM
+console.log(document.body);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes[1].textContent);
