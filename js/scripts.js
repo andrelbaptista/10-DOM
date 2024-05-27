a961344722e8a04d07165940ae66278ab9d50786
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-05-27 16:14:45 -0300

    Chapter end

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,17 @@ console.log(document.body.childNodes[1].childNodes[1]);
 
 console.log(document.body.childNodes[1].childNodes[1].textContent);
 
-
-
-
 // selecionando elementos por tag
 const listaItens = document.getElementsByTagName("li");
 console.log(listaItens);
-
-
 
 // selecionando o elemento por id
 const titulo = document.getElementById("title");
 console.log(titulo);
 
-
-
-
 // selecionando elementos por classe
 const listaProdutos = document.getElementsByClassName("product");
 console.log(listaProdutos);
-
-
 
 // selecionando elemento por CSS
 const produtoClasse = document.querySelectorAll(".product");
@@ -38,8 +28,6 @@ console.log(produtoClasse);
 
 const conteudoPrincipal = document.querySelector("#main-container");
 console.log(conteudoPrincipal);
-
-
 
 // insertBefore
 const newP = document.createElement("p");
@@ -50,17 +38,12 @@ console.log(parent);
 
 parent.insertBefore(newP, insertPoint);
 
-
-
-
 // appendChild
 const navLinkParent = document.querySelector("nav ul");
 console.log(navLinkParent);
 
 const novoLi = document.createElement("li");
 navLinkParent.appendChild(novoLi);
-
-
 
 // replaceChild
 const insertReplace = document.querySelector("#title");
@@ -74,9 +57,6 @@ titleReplace.textContent = "Novo TÍTULO !!!";
 
 titleParent.replaceChild(titleReplace, insertReplace);
 
-
-
-
 // createTextNode
 const newText = document.createTextNode("Novo tag h3 com título");
 console.log(newText);
@@ -88,10 +68,7 @@ console.log(subTitle);
 const containerParent = document.querySelector("#main-container");
 console.log(containerParent);
 
-containerParent.appendChild(subTitle)
-
-
-
+containerParent.appendChild(subTitle);
 
 // getAttribute and setAttribute
 const firstLinkUpdate = document.querySelector("a");
@@ -101,10 +78,7 @@ firstLinkUpdate.setAttribute("href", "https://www.deeptech.com.br");
 
 console.log(firstLinkUpdate.getAttribute("href"));
 
-firstLinkUpdate.setAttribute("target","_blank");
-
-
-
+firstLinkUpdate.setAttribute("target", "_blank");
 
 // offsetWidth offsetHeight clientWidth clientHeight
 const footerSection = document.querySelector("footer");
@@ -113,19 +87,24 @@ console.log(footerSection);
 console.log(footerSection.offsetWidth, footerSection.offsetHeight);
 console.log(footerSection.clientWidth, footerSection.clientHeight);
 
-
-
-
-
 // getBoudingClientRect
 const itens = document.querySelectorAll(".product");
 console.log(itens);
 
-const firstProduct = (itens[0]);
+const firstProduct = itens[0];
 console.log(firstProduct);
 console.log(firstProduct.getBoundingClientRect());
 
+// CSS and JS
+const mainContent = document.querySelector("#main-container");
+console.log(mainContent);
 
+mainContent.style.color = "red";
+mainContent.style.paddingBottom = "15px";
 
-
-// 
+// Changing multiple styles
+const unformatListParent = document.getElementsByTagName("li");
+console.log(unformatListParent);
+for (const i of unformatListParent) {
+i.style.backgroundColor = "red";
+}
